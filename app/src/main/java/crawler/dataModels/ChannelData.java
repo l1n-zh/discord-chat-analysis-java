5b8b56823402331658449fd03991be654cb7fc70
L1n <89698082+l1n-zh@users.noreply.github.com>
2024-06-16 00:49:56 +0800

    remove magic number

--- app/src/main/java/crawler/dataModels/ChannelData.java
+++ app/src/main/java/crawler/dataModels/ChannelData.java
@@ -3,7 +3,6 @@ package crawler.dataModels;
 import java.util.ArrayList;
 
 public class ChannelData {
-    // public String lastUpdateMessageID = "1000000000000000000";
-    public String lastUpdateMessageID = "886902195776258099";
+    public String lastUpdateMessageID = "";
     public ArrayList<MessageData> messages = new ArrayList<>();
 }
