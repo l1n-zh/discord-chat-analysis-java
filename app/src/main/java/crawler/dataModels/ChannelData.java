e8d3d940dbac9184d96707f21b63d298c1fefd02
L1n <89698082+l1n-zh@users.noreply.github.com>
2024-06-16 00:54:21 +0800

    fix: message ID must be snowflake format

--- app/src/main/java/crawler/dataModels/ChannelData.java
+++ app/src/main/java/crawler/dataModels/ChannelData.java
@@ -3,6 +3,6 @@ package crawler.dataModels;
 import java.util.ArrayList;
 
 public class ChannelData {
-    public String lastUpdateMessageID = "";
+    public String lastUpdateMessageID = "0000000000000000000";
     public ArrayList<MessageData> messages = new ArrayList<>();
 }
