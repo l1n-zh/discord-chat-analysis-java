6894aeeedd74a81e1d4263accf09146209c553ef
YunshiuanOAO <yunshiuan1101@gmail.com>
2024-06-17 21:35:16 +0800

    update clawer

--- app/src/main/java/crawler/DataSet.java
+++ app/src/main/java/crawler/DataSet.java
@@ -4,27 +4,26 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import crawler.dataModels.ChannelData;
+import crawler.dataModels.ExternalData;
+import crawler.dataModels.AllExternalData;
 import crawler.dataModels.MessageData;
 import net.dv8tion.jda.api.entities.Message;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class DataSet {
 
     private HashMap<String, ChannelData> dataSet;
+    private AllExternalData externalData = new AllExternalData();
     private File dataFile;
     private static ObjectMapper objectMapper = new ObjectMapper();
 
-
-
     DataSet(File dataFile) throws IOException {
         this.dataFile = dataFile;
-
         if (dataFile.exists()) {
             try {
                 this.dataSet = objectMapper.readValue(dataFile, new TypeReference<HashMap<String, ChannelData>>() { });
@@ -48,6 +47,21 @@ public class DataSet {
         dataSet.get(channelID).messages.add(messageData);
         dataSet.get(channelID).lastUpdateMessageID = message.getId();
     }
+    public void addExternalChannel(String channelId,String channelName,String channelCategoryName) {
+        ExternalData tmp = new ExternalData();
+        tmp.title = channelName;
+        tmp.subtitle = channelCategoryName;
+        tmp.value = channelId;
+        externalData.channels.add(tmp);
+
+    }
+    public void addExternalUser(String userId,String userName,String usersubname) {
+        ExternalData tmp = new ExternalData();
+        tmp.title = userName;
+        tmp.subtitle = usersubname;
+        tmp.value = userId;
+        externalData.users.add(tmp);
+    }
 
     public ChannelData getChannelData(String channelID) {
         return dataSet.getOrDefault(channelID, new ChannelData());
@@ -55,6 +69,13 @@ public class DataSet {
 
     public void save() throws IOException {
         String jsonString = objectMapper.writeValueAsString(dataSet);
+        FileWriter writer = new FileWriter(dataFile);
+        writer.write(jsonString);
+        writer.close();
+    }
+    public void saveExternalData() throws IOException {
+
+        String jsonString = objectMapper.writeValueAsString(externalData);
         FileWriter writer = new FileWriter(dataFile);
         writer.write(jsonString);
         writer.close();
